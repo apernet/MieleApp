de04df037cf28e0f1095ffe577078efc74d6cce0
Daniel Luna <dluna@aper.net>
2017-02-10 18:43:39 -0600

    adding css to survey interface

--- public_html/js/surveyInterface.js
+++ public_html/js/surveyInterface.js
@@ -1,6 +1,7 @@
 define(['jquery', 'surveys', 'system', 'surveyBuilder', 'jquery.bxslider', 'home'], function($, Surveys, sys, SurveyBuilder, bx, menu) {
     var SurveyInterface = function() {
         var token = null;
+        var slider;
         this.init = function() {
             token = sys.getUrlParameter("token");
             buildMenu();
@@ -23,17 +24,19 @@ define(['jquery', 'surveys', 'system', 'surveyBuilder', 'jquery.bxslider', 'home
         var showSurvey = function() {
             var survey = getSurvey();
             var sliderContainer;
-
+            
             survey = ($.isPlainObject(survey)) ? null : survey[0];
             console.log(survey);
+            
+            hideNavBarTitle();
+            
+            setTitle(survey.name);
 
             setWelcomText(survey);
 
             SurveyBuilder.init({token: token}, survey);
 
             $(survey.mst_questions).each(function(index) {
-                if (index % 3 === 0)
-                    console.log(index);
                 /* add a new slider each three questions */
                 if (index % 3 === 0)
                     sliderContainer = SurveyBuilder.addQuestion(this);
@@ -43,15 +46,45 @@ define(['jquery', 'surveys', 'system', 'surveyBuilder', 'jquery.bxslider', 'home
             });
 
             setFinishText(survey);
-
-            $('#slider').bxSlider({
+            
+            setButtonsNext();
+            
+            slider = $('#slider').bxSlider({
                 mode: "fade",
                 speed: 800,
                 hideControlOnEnd: true,
                 pager: false,
                 slideSelector: $(".slider-container"),
-                infiniteLoop: false
+                infiniteLoop: false,
+                controls: false,
+                touchEnabled: false,
+                onSlideNext: function($slideElement, oldIndex, newIndex){
+                    showNavBarTitle();
+                }
             });
+        };
+        
+        /**
+         * @description Set button next in each slider
+         * @returns {undefined}
+         */
+        var setButtonsNext = function(){
+            $('.slider-container').each(function(){
+                $(this).append(getButtonNext("siguientee"));
+            });
+        };
+        
+        var showNavBarTitle = function(){
+            $('.navbar').removeClass('message-mode');
+        };
+        
+        var hideNavBarTitle = function(){
+            $('.navbar').addClass('message-mode');
+        };
+        
+        var setTitle = function(title){
+            $('.survey-title').append(title);
+            
         };
 
         var getFooter = function() {
@@ -59,16 +92,17 @@ define(['jquery', 'surveys', 'system', 'surveyBuilder', 'jquery.bxslider', 'home
         };
 
         var setWelcomText = function(survey) {
-            var content = $('<div>', {class: "slider-container message-container", id: "question_welcome_text", idQuestionType: 0, idQuestion: 0})
-                    .append(survey.welcome_text)
-                    .append(getFooter());
+            var content = $('<div>', {class: "message-container", id: "question_welcome_text", idQuestionType: 0, idQuestion: 0})
+                    .append($('<div>', {class:"text-wrapper"}).append(survey.welcome_text))
+                    .append(getFooter())
+                    .append(getButtonNext("Empezar"));
 
             $('#slider').append(content);
         };
 
         var setFinishText = function(survey) {
-            var content = $('<div>', {class: "slider-container message-container ", id: "question_welcome_text", idQuestionType: 0, idQuestion: 0})
-                    .append(survey.finish_text)
+            var content = $('<div>', {class: "message-container", id: "question_finish_text", idQuestionType: 0, idQuestion: 0})
+                    .append($('<div>', {class:"text-wrapper"}).append(survey.finish_text))
                     .append(getFooter());
 
             $('#slider').append(content);
@@ -78,6 +112,15 @@ define(['jquery', 'surveys', 'system', 'surveyBuilder', 'jquery.bxslider', 'home
             var idSurvey = sys.getUrlParameter("idSurvey");
 
             return (!parseInt(idSurvey) > 0) ? null : Surveys.getSurvey(token, idSurvey);
+        };
+
+        var getButtonNext = function(text) {
+            var button = $('<div>', {class: "text-center next-bx", id: "next-bx"}).append($('<button>', {class: "btn btn-primary btn-lg"}).append(text));
+            button.on("click", function() {
+                    slider.goToNextSlide();
+                    return false;
+            });
+            return button;
         };
 
     };
